83d2a6b5d91d597456ce1e52c1ac349aedbc7755
720723110063-coder <720723110063@hicet.ac.in>
2026-07-07 12:54:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function showMessage() {
-    alert("Welcome to My Portfolio Website!");
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value.trim();
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        <span onclick="toggleTask(this)" style="cursor:pointer;">${task}</span>
+        <button class="delete" onclick="deleteTask(this)">Delete</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
+
+function toggleTask(task) {
+    if (task.style.textDecoration === "line-through") {
+        task.style.textDecoration = "none";
+    } else {
+        task.style.textDecoration = "line-through";
+    }
 }
